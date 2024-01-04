3c1a8bc7e2670abcbb13f512d8a8cc11ab605aa2
Lab1-1 <Lab1-1@DESKTOP-NHSN8JG>
2024-01-04 09:55:56 +0300

    Proje dosyası ekle.

---
+++ BGT_Gorsel_4Oca-1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BGT_Gorsel_4Oca_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ekran2 ikinciEkran = new Ekran2();
+            ikinciEkran.ShowDialog();
+            
+        }
+    }
+}
